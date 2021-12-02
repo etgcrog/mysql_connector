48e9d7779ee2fc38069531346dd24729b6acff9b
Eduardo Teles <61814067+etgcrog@users.noreply.github.com>
2021-12-02 15:32:15 -0300

    Add files via upload
    melhorias

--- connector.py
+++ connector.py
@@ -15,16 +15,37 @@ class Connect:
         client = MongoClient(self.localhost)
         db = client[self.database]
         collection = db[self.collection]
-        print(f"BANCO {self.database} selecionado na collection {self.collection}")
         return collection
 
     def insert_one(self, arg):
         self.connectar().insert_one(arg)
         print("Inserido com sucesso!")
         
+    def insert_many(self, lista):
+        # list_item = []
+        # item1 = Item("Eduardo", 21).to_dict()
+        # item2 = Item("Anselmo", 22).to_dict()
+        # item3 = Item("James", 23).to_dict()
+        
+        # list_item.append(item1)
+        # list_item.append(item2)
+        # list_item.append(item3)
+        
+        # c2.insert_many(list_item)
+        self.connectar().insert_many(lista)
+        
     def select_one(self):
         pprint.pprint(self.connectar().find_one())
         
+    def select_all(self):
+        #tem que iterear para funcionar
+        details = self.connectar().find()
+        for n in details:
+            print(n)
+
+    def drop_collection(self):
+        self.connectar().drop()
+    
     
 
     
--- item.py
+++ item.py
@@ -1,7 +1,14 @@
 class Item:
     #Nao esquecer de fazer com args e kwargs
-    def __init__(self, ):
-        self.args = args 
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        
+    def get_nome(self):
+        return self.nome
+        
+    def get_idade(self):
+        return self.idade
         
     def to_dict(self):
-        return {"nome":self.args, "atributos":self.kwargs}
+        return {"nome": self.get_nome(), "idade":self.get_idade()}
